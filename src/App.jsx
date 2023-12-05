d9ff1962260d376285cc3afaaa12e927cdc76945
mayankbankoti <mayank.bankoti@netprophetsglobal.com>
2023-12-05 10:22:29 +0530

    first changes commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+
+const App = () =>{
+
+  return(
+    <>
+      <Header />
+      // <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
